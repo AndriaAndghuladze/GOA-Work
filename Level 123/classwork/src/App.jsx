bef18c53321bfb30693446f68bc530b85a3cddc2
AndriaAndghuladze <andriaand999@gmail.com>
2025-12-26 23:18:16 +0400

    commit

---
+++ Level 123/classwork/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div>
+      <p className="bg-red-500 text-white ">
+        Hello World
+      </p>
+
+      <p className="bg-blue-500 text-white ">
+        Hello World1
+      </p>
+
+      <p className="bg-green-500 text-black ">
+        Hello World2
+      </p>
+    </div>
+  );
+}
