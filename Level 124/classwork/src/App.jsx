847cdad8445aab5ae4f60f316893883e63972e20
AndriaAndghuladze <andriaand999@gmail.com>
2025-12-28 19:57:01 +0400

    commit

---
+++ Level 124/classwork/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App(){
+  return(
+    <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 1</button>
+      <button className='bg-color-blue text-color-black'>Click Me 2</button>
+      <button className='bg-color-green text-color-black'>Click Me 3</button>
+      
+    </div>
+  )
+}
