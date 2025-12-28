aa69a9f1b828d2a9538381c487241fc0f41b2d9a
AndriaAndghuladze <andriaand999@gmail.com>
2025-12-28 20:02:56 +0400

    commit

---
+++ Level 124/homework/src/App.jsx
@@ -0,0 +1,54 @@
+export default function App(){
+  return(
+    <div>
+
+      <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 1</button>
+      <button className='bg-color-blue text-color-black'>Click Me 2</button>
+      <button className='bg-color-green text-color-black'>Click Me 3</button>
+
+      </div>
+
+
+      <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 4</button>
+      <button className='bg-color-blue text-color-black'>Click Me 5</button>
+      <button className='bg-color-green text-color-black'>Click Me 6</button>
+
+      </div>
+
+
+      <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 7</button>
+      <button className='bg-color-blue text-color-black'>Click Me 8</button>
+      <button className='bg-color-green text-color-black'>Click Me 9</button>
+
+      </div>
+
+      <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 10</button>
+      <button className='bg-color-blue text-color-black'>Click Me 11</button>
+      <button className='bg-color-green text-color-black'>Click Me 12</button>
+
+      </div>
+
+
+      <div className="flex justify-center itemს-center  h-[100vh] ">
+
+      <button className='bg-color-red text-color-black'>Click Me 13</button>
+      <button className='bg-color-blue text-color-black'>Click Me 14</button>
+      <button className='bg-color-green text-color-black'>Click Me 15</button>
+
+      </div>
+
+
+    //Flex-ის საშიუალებით ჩვენ შეგვიძლია ელემენტებს საიტზე შევურჩიოთ ადგილი X და Y ღერძებზე
+
+      
+    </div>
+  )
+}
