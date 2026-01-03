d82737e6495a3ed794b50d6f2331a09632641dee
AndriaAndghuladze <andriaand999@gmail.com>
2026-01-04 01:59:15 +0400

    commit

---
+++ Level 125/homework/src/App.jsx
@@ -0,0 +1,69 @@
+import mt from "./assets/photo.png";
+
+export default function App(){
+  return (
+    <div>
+      <img src={mt} alt="mt" className="w-3/4 sm:w-1/2 lg:w-1/4" />
+
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800">Thise is mountine</h1>
+
+        <button className="bg-black text-white  md: w-48 rounded-20 ">
+          Click Me
+        </button>
+
+        {/* ............... */}
+
+        <img src={mt} alt="mt" className="w-3/4 sm:w-1/2 lg:w-1/4" />
+
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800">Thise is mountine</h1>
+
+        <button className="bg-black text-white  md: w-48 rounded-20 ">
+          Click Me
+        </button>
+
+        {/* ............... */}
+
+        <img src={mt} alt="mt" className="w-3/4 sm:w-1/2 lg:w-1/4" />
+
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800">Thise is mountine</h1>
+
+        <button className="bg-black text-white  md: w-48 rounded-20 ">
+          Click Me
+        </button>
+
+        {/* ............... */}
+
+        <img src={mt} alt="mt" className="w-3/4 sm:w-1/2 lg:w-1/4" />
+
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800">Thise is mountine</h1>
+
+        <button className="bg-black text-white  md: w-48 rounded-20 ">
+          Click Me
+        </button>
+
+        {/* ............... */}
+
+        <img src={mt} alt="mt" className="w-3/4 sm:w-1/2 lg:w-1/4" />
+
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-gray-800">Thise is mountine</h1>
+
+        <button className="bg-black text-white  md: w-48 rounded-20 ">
+          Click Me
+        </button>
+
+        {/* ............... */}
+
+        {/* 
+        
+          sm - 40rem (640px)- Minimum width 
+          md - 48rem (768px)- Minimum width 
+          lg -	64rem (1024px)-Minimum width 
+          xl -	80rem (1280px)-Minimum width 
+          2xl -	96rem (1536px)-Minimum width 
+          tailwind-ში გვაქვს Breakpoint-ი რომელიც განსაზღვრავს რესპონსივს
+          
+
+        */}
+    </div>
+  )
+}
