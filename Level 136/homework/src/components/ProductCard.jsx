a6090454362e6cad9fb4b3869be9df4b6fb626ae
AndriaAndghuladze <andriaand999@gmail.com>
2026-02-14 19:05:51 +0400

    commit

--- Level 136/homework/src/components/ProductCard.jsx
+++ Level 136/homework/src/components/ProductCard.jsx
@@ -3,7 +3,6 @@ import "./ProductCard.css";
 function ProductCard({ product }) {
   return (
     <div className="card">
-      <img src={product.image} alt={product.name} className="card-img" />
       <h3 className="card-title">{product.name}</h3>
       <p className="card-price">{product.price}</p>
       <button className="card-btn">View</button>
