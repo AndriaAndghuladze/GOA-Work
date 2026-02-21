3383298762559f2d04ffd3f0615df04dfe715dc3
AndriaAndghuladze <andriaand999@gmail.com>
2026-02-21 21:26:12 +0400

    commit

---
+++ Level 139/homework/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          console.error("Fetch error:", err);
+        }
+      });
+
+    
+    return () => {
+      controller.abort();
+    };
+  }, []); 
+
+  return (
+    <>
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.id}</p>
+          <p>{user.name}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
+
+//We use Hook before we return another code
