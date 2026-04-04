9d2b4dd0ddf4ecc14b99b337719f6931350975fa
AndriaAndghuladze <andriaand999@gmail.com>
2026-04-04 20:46:17 +0400

    commit

--- Level 150/classwork/src/contact.jsx
+++ Level 150/classwork/src/contact.jsx
@@ -1,7 +1,9 @@
 export default function App(){
   return(
     <div>
-        <h1>Contact Page</h1>
+        <h1 className="text-blue-500">Contact Page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab sequi atque ipsa magnam voluptatibus, repellat soluta, repellendus odit vel odio libero. Excepturi quaerat ipsum consequuntur obcaecati, natus voluptates rem.</p>
     </div>
   )
 }
